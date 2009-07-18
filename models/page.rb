a34c72351cfe6b48046eb63596b9031b56177d5d
Bruce <brucewoodward@bruce-woodwards-macbook.local>
2009-07-18 17:20:02 +1000

    Broken; bacon doesn't give a stack trace

--- models/page.rb
+++ models/page.rb
@@ -1,7 +1,13 @@
 class Page
+
+  @@data_store = "/Users/brucewoodward/code/biki/biki_files/"
+
   class << self
-    def find_or_create_by_name(name)
-      find_by_name(name) || new(name).save
+    def load(name)
+      [ @@data_store + name, @@data_store + "#{name}_tags" ].each do |filename|
+        File.new(filename, "a").close
+      end
+      new name
     end
   end
   
@@ -9,16 +15,32 @@ class Page
   attr_accessor :content
 
   def initialize(name)
+    @content, @tags = [], []
+    @name = name
+    File.open(@@data_store + name) do |file|
+      @content = file.readlines
+    end
+    File.open(@@data_store + name + '_tags') do |file|
+      file.each_line do |line|
+        @tags << line.chomp
+      end
+    end
   end
 
   def save
-    self
+    File.open(@@data_store + @name, "w") do |file|
+      @content.each do |line|
+        file.write line
+      end
+    end
   end
 
   def content
+    @content
   end
   
   def tags
+    @tags
   end
 
 end
--- test/test_biki.rb
+++ test/test_biki.rb
@@ -1,16 +1,24 @@
 require File.expand_path(File.dirname(__FILE__) + '/test_helper')
 
-describe "Bruce's Bodgey Wiki" do
-  describe 'Happy Happy Joy Joy' do
-    it "good to be alive" do # nothing_really
-      true
+describe 'Testing page model' do
+  it 'should create a new file' do 
+    file_name = Page.data_store + 'testing'
+    File.unlink(file_name)
+    page = Page.load 'testing'
+    page.content = 'more testing'
+    page.save
+    File.exists(file_name).should.be.true
+    lines = Array.new
+    File.open(file_name) do |file|
+      lines = file.readlines
     end
+    lines.size.should.be 1
+    lines[0].chomp!
+    lines[0].should.equal = "more testing"
   end
-
-  describe "Create a new page" do
-    it "should rain more often" do
-      true
-    end
+  it 'load the existing file' do
   end
-
+  it 'save changes' do
+  end
 end
+
